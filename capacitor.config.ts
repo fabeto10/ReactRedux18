c0497f9e9aaa6c266fb9bb17df5197c6d5109445
fabeto10 <f0414475@gmail.com>
2023-01-04 14:55:17 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tourR0.1',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
